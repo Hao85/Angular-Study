1e7ac1d317eabe1c98a5858db94da4da2aeb4e45
hao85 <haozhang@sparqsys.com>
2020-12-16 18:31:38 -0500

    Prepare for Authentication and Authorization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { fakeBackendProvider } from './helpers/fake-backend';
+import { OrderService } from './services/order.service';
 import { GithubFollowersService } from './services/github-followers.service';
 import { AppErrorHandler } from './common/app-error-handler';
 import { PostService } from './services/post.service';
@@ -27,7 +29,12 @@ import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { GithubProfileComponent } from './github-profile/github-profile.component';
-import { RouterModule } from "@angular/router";
+import { Router, RouterModule } from "@angular/router";
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+import { NoAccessComponent } from './no-access/no-access.component';
+import { SignupComponent } from './signup/signup.component';
+import { AuthService } from './services/auth.service';
 
 @NgModule({
   declarations: [
@@ -48,7 +55,11 @@ import { RouterModule } from "@angular/router";
     HomeComponent,
     NotFoundComponent,
     NavbarComponent,
-    GithubProfileComponent
+    GithubProfileComponent,
+    AdminComponent,
+    LoginComponent,
+    NoAccessComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
@@ -59,32 +70,54 @@ import { RouterModule } from "@angular/router";
     ToastrModule.forRoot(),
     RouterModule.forRoot([
       {
-        path: "", 
+        path: "",
         component: HomeComponent
       },
       {
-        path: "followers/:id/:username", 
-        component: GithubProfileComponent
+        path: "admin",
+        component: AdminComponent
       },
       {
-        path: "followers", 
-        component: GithubFollowersComponent
+        path: "login",
+        component: LoginComponent
       },
       {
-        path: "posts", 
-        component: PostsComponent
-      },
-      {
-        path: "**", 
-        component: NotFoundComponent
-      },
+        path: "no-acess",
+        component: NoAccessComponent
+      }
     ])
+    // RouterModule.forRoot([
+    //   {
+    //     path: "", 
+    //     component: HomeComponent
+    //   },
+    //   {
+    //     path: "followers/:id/:username", 
+    //     component: GithubProfileComponent
+    //   },
+    //   {
+    //     path: "followers", 
+    //     component: GithubFollowersComponent
+    //   },
+    //   {
+    //     path: "posts", 
+    //     component: PostsComponent
+    //   },
+    //   {
+    //     path: "**", 
+    //     component: NotFoundComponent
+    //   },
+    // ])
   ],
   providers: [
     AuthorService,
     PostService,
     GithubFollowersService,
-    {provide: ErrorHandler, useClass: AppErrorHandler}
+    {provide: ErrorHandler, useClass: AppErrorHandler},
+    OrderService,
+    AuthService,
+    // for creating a  mock back-end. you don't need these in a real app
+    fakeBackendProvider,
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/helpers/fake-backend.ts
@@ -0,0 +1,75 @@
+import {
+    HttpRequest,
+    HttpHandler,
+    HttpEvent,
+    HTTP_INTERCEPTORS,
+    HttpResponse,
+    HttpInterceptor,
+  } from "@angular/common/http";
+  import { Observable, of } from "rxjs";
+  import { mergeMap, materialize, dematerialize, delay } from "rxjs/operators";
+  
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gV2FuZyIsImFkbWluIjp0cnVlfQ.Y61CwMOJ92BgJTqXGQeACPCB03R9UXely69_3YG6cyY";
+  
+  export class FakeBackendInterceptor implements HttpInterceptor {
+    intercept(
+      request: HttpRequest<any>,
+      next: HttpHandler
+    ): Observable<HttpEvent<any>> {
+      const { url, method, headers, body } = request;
+      // wrap in delayed obervable to simulate server api call
+      return of(null)
+        .pipe(mergeMap(handleRoute))
+        .pipe(materialize())
+        .pipe(delay(1000))
+        .pipe(dematerialize());
+  
+      function handleRoute() {
+        switch (true) {
+          case url.endsWith("/api/authenticate") && method === "POST":
+            return authenticate();
+          case url.endsWith("/api/orders") && method === "GET":
+            return orders();
+          default:
+            // pass through any requests not handled above
+            return next.handle(request);
+        }
+      }
+  
+      function authenticate() {
+        const { email, password } = JSON.parse(body);
+        if (email === "john@domain.com" && password === "1234")
+          return ok({ token: token });
+        else return badRequest();
+      }
+  
+      function orders() {
+        if (headers.get("Authorization") === "Bearer " + token)
+          return ok([1, 2, 3]);
+        else return unauthorized();
+      }
+  
+      function ok(body?: any) {
+        return of(new HttpResponse({ status: 200, body }));
+      }
+  
+      function unauthorized() {
+        // 401: unAuthorized
+        return of(new HttpResponse({ status: 401 }));
+      }
+  
+      function badRequest() {
+        // 400: bad request
+        return of(new HttpResponse({ status: 400 }));
+      }
+    }
+  }
+  
+  export const fakeBackendProvider = {
+    // use fake backend in place of Http service for backend-less development
+    provide: HTTP_INTERCEPTORS,
+    useClass: FakeBackendInterceptor,
+    multi: true,
+  };
+  
